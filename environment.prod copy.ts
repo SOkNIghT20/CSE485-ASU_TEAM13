da1ee46325e13a0a3547db0ace1a44a877acee80
Sonit Sai Penchala <sonit@Sonits-MacBook-Air.local>
2025-10-15 22:33:57 -0700

    Wipe repo clean

--- environment.prod copy.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  serverUrl: 'http://3.13.169.109:80',
-  apiBase: 'http://3.13.169.109:80',
-  downloadBase: 'http://3.13.169.109:80',
-  termsOfUseDocumentName: 'TERMSOFUSE.pdf'
-};
